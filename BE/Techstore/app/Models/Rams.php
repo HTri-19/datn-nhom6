3bf67f4c8479a3a1328054651b50f4a271a82150
Nguyễn Hoàng Kha <kha@example.com>
2025-10-31 15:26:51 +0700

    BE Html css

---
+++ BE/Techstore/app/Models/Rams.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Product_variants;
+class Rams extends Model
+{
+    use HasFactory;
+     protected $table = 'rams';
+    protected $fillable = ['name', 'value'];
+
+    public function variants()
+    {
+        return $this->hasMany(Product_variants::class, 'ram_id');
+    }
+}
